ed1d784e394aac98a3b56620e58b31a29db4d856
AnnaTabaka <AnnaTabaka@users.noreply.github.com>
2016-04-08 13:21:10 +0200

    Added files via upload

---
+++ Window.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Words
+{
+    public partial class Window : Form
+    {
+        #region Ctors
+
+        public Window()
+        {
+            InitializeComponent();
+            //Example sentence
+            rtxSoruce.Text = @"Example: Time times time times time squared equals time times time times time times time.";
+        }
+
+        #endregion Ctors
+        #region Private methods
+
+        //Write list of stem of words, words with the same stem in brackets and its occurrence to 'output'
+        private void StemmedWords()
+        {
+            var wordCount = new WordCount();
+            wordCount.CountStemmedWords(rtxSoruce.Text);
+
+            var text = string.Empty;
+            foreach (var word in wordCount.CountedWords)
+            {
+                var stemmedWords = string.Join(", ", wordCount.StemmedWords[word.Key].Select(n => n.ToString()));
+                text = string.Format("{0}{1} ({3}) - {2}\n", text, word.Key, wordCount.CountedWords[word.Key],
+                    stemmedWords);
+            }
+            rtxResult.Text = text;
+        }
+
+        //Write list of words and its occurrence to 'output'
+        private void Words()
+        {
+            var wordCount = WordCount.CountWords(rtxSoruce.Text);
+            var text = wordCount.Aggregate(string.Empty,
+                (t, word) => string.Format("{0}{1} - {2}\n", t, word.Key, wordCount[word.Key]));
+            rtxResult.Text = text;
+        }
+
+        #endregion Private methods
+        #region Event handlers
+
+        //Clear 'input'
+        private void butClear_Click(object sender, EventArgs e)
+        {
+            rtxSoruce.Clear();
+        }
+
+        //Clear 'input' and 'output'
+        private void butClearAll_Click(object sender, EventArgs e)
+        {
+            rtxSoruce.Clear();
+            rtxResult.Clear();
+        }
+
+        //Write list of words and its occurrence to 'output'
+        private void butCountBasic_Click(object sender, EventArgs e)
+        {
+            Words();
+        }
+
+        //Write list of stem of words, words with the same stem in brackets and its occurrence to 'output'
+        private void butCountStem_Click(object sender, EventArgs e)
+        {
+            StemmedWords();
+        }
+
+        #endregion Event handlers
+    }
+}
---
+++ WordCount.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Words
+{
+    class WordCount
+    {
+        #region Fields
+
+        public Dictionary<string, int> CountedWords;
+        public Dictionary<string, HashSet<string>> StemmedWords;
+
+        #endregion Fields
+        #region Public methods
+
+        //Create a dictionary (word, it's occurence) from input sentence
+        public static Dictionary<string, int> CountWords(string sentence)
+        {
+            var countedWords = new Dictionary<string, int>();
+            var words = SplitSentence(sentence);
+
+            foreach (var word in words)
+            {
+                AddValue(countedWords, word);
+            }
+
+            return countedWords;
+        }
+
+        //Create a dictionary (word, it's occurence)
+        //and a dictionary (stem of word, list of words with the same stem) from input sentence
+        public void CountStemmedWords(string sentence)
+        {
+            CountedWords = new Dictionary<string, int>();
+            StemmedWords = new Dictionary<string, HashSet<string>>();
+            var words = SplitSentence(sentence);
+
+            foreach (var word in words)
+            {
+                var stemmedWord = Stem(word);
+
+                AddValue(CountedWords, stemmedWord);
+                AddValue(StemmedWords, stemmedWord, word);
+            }
+        }
+
+        #endregion Public methods
+        #region Private methods
+
+        //Create a list of words from input sentence
+        private static IEnumerable<string> SplitSentence(string sentence)
+        {
+            sentence = sentence.ToLower();
+            var words = new List<string>();
+
+            var sb = new StringBuilder();
+            foreach (var c in sentence)
+            {
+                if (char.IsLetterOrDigit(c) || c.Equals('\''))
+                {
+                    sb.Append(c);
+                }
+                else
+                {
+                    if (sb.Length > 0)
+                    {
+                       words.Add(sb.ToString());
+                    }
+                    sb = new StringBuilder();
+                }
+            }
+            if (sb.Length > 0)
+            {
+                words.Add(sb.ToString());
+            }
+
+            return words;
+        }
+
+        //The Porter2 stemming algorithm, author of the class in Porter2.cs: Kamil Bartocha
+        private static string Stem(string word)
+        {
+            var stemmer = new Porter2();
+            return stemmer.stem(word);
+        }
+
+        //Add word and its occurrence to dictionary (word, it's occurence)
+        private static void AddValue(Dictionary<string, int> countedWords, string word)
+        {
+            int count;
+            if (countedWords.TryGetValue(word, out count))
+            {
+                countedWords[word] = count + 1;
+            }
+            else
+            {
+                countedWords.Add(word, 1);
+            }
+        }
+
+        //Add stem of word and word with the same stem to dictionary (stem of word, list of words with the same stem)
+        private static void AddValue(Dictionary<string, HashSet<String>> stemmedWords, string stemmedWord, string word)
+        {
+            HashSet<string> hashSet;
+            if (stemmedWords.TryGetValue(stemmedWord, out hashSet))
+            {
+                hashSet.Add(word);
+            }
+            else
+            {
+                hashSet = new HashSet<string>{word};
+                stemmedWords.Add(stemmedWord, hashSet);
+            }
+        }
+
+        #endregion Private methods
+    }
+}
